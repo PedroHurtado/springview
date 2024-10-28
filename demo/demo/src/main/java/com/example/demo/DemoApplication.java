c676a3791b507d8366660d90147ce4ed62f59e68
Pedro Hurtado <pedrohurt@gmail.com>
2024-10-28 19:36:07 +0100

    add repo

--- demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//http://localhost:8080/h2-console
+
 @SpringBootApplication
 public class DemoApplication {
 
